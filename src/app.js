27f54943ef2c4bc9663cf0dbadf959ab9473b43d
Denis Sitnikov <dmrock@Deniss-iMac.local>
2021-08-21 18:39:45 +0300

    Created bg module

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import './styles.css'
 import {ContextMenu} from "@/menu"
 import {TestModule} from "@/modules/test.module"
 import {SoundModule} from "@/modules/sound.module";
+import {BackgroundModule} from "@/modules/background.module";
 
 const contextMenu = new ContextMenu("#menu")
 
 contextMenu.add(new TestModule('test', 'Тест'))
 contextMenu.add(new SoundModule('sound', 'Случайный звук'))
+contextMenu.add(new BackgroundModule('bg', 'Случайный фон'))
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
 import {Module} from '../core/module'
+import {random} from "@/utils";
 
 export class BackgroundModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+        this.backgrounds = [
+            '#A5FF8A',
+            '#E0DC78',
+            '#FA685A',
+            '#884EE0',
+            '#6BF6FF',
+            '#FAA73F'
+        ]
+    }
 
+    trigger() {
+        document.body.style.background = this.backgrounds[random(0, this.backgrounds.length)]
+    }
 }
