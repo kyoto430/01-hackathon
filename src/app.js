8b808bc47a344f813a00033361ad68d56c7cf9a9
Vera <versuchi@yandex.ru>
2021-08-22 19:34:26 +0300

    Implemented timer module

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import './styles.css'
-import { ContextMenu } from '@/menu'
-import { SoundModule } from '@/modules/sound.module'
-import { BackgroundModule } from '@/modules/background.module'
-import { ShapeModule } from '@/modules/shape.module'
-import { CustomMessageModule } from '@/modules/custom-message.module'
+import "./styles.css";
+import { ContextMenu } from "@/menu";
+import { SoundModule } from "@/modules/sound.module";
+import { BackgroundModule } from "@/modules/background.module";
+import { ShapeModule } from "@/modules/shape.module";
+import { CustomMessageModule } from "@/modules/custom-message.module";
+import { TimerModule } from "@/modules/timer.module";
 
-const contextMenu = new ContextMenu('#menu')
+const contextMenu = new ContextMenu("#menu");
 
-contextMenu.add(new SoundModule('sound', 'Случайный звук'))
-contextMenu.add(new BackgroundModule('bg', 'Случайный фон'))
-contextMenu.add(new ShapeModule('shape', 'Случайная фигура'))
-contextMenu.add(new CustomMessageModule('message', 'Случайное сообщение'))
+contextMenu.add(new SoundModule("sound", "Случайный звук"));
+contextMenu.add(new BackgroundModule("bg", "Случайный фон"));
+contextMenu.add(new ShapeModule("shape", "Случайная фигура"));
+contextMenu.add(new CustomMessageModule("message", "Случайное сообщение"));
+contextMenu.add(new TimerModule("timer", "Отсчет таймера"));
