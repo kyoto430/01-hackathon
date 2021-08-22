c847e03a562c7e0709f1e98d0eefdcbeccffcace
Ruslan Prakapets <nateriver345@gmail.com>
2021-08-22 13:06:27 +0300

    modification message-module

--- src/core/constants/settings.js
+++ src/core/constants/settings.js
@@ -1,8 +1,17 @@
 export const randomColors = [
-    '#A5FF8A',
-    '#E0DC78',
-    '#FA685A',
-    '#884EE0',
-    '#6BF6FF',
-    '#FAA73F'
+  '#A5FF8A',
+  '#E0DC78',
+  '#FA685A',
+  '#884EE0',
+  '#6BF6FF',
+  '#FAA73F',
+]
+
+export const randomMessages = [
+  'Hello World',
+  'Java Script',
+  'True or false?',
+  'Awesome!!!',
+  'Not yet',
+  'Try again',
 ]
