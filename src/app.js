8be76ec917e423eda66f1ce5676412625fc63aca
Ruslan Prakapets <nateriver345@gmail.com>
2021-08-22 10:06:32 +0300

    clicks-module +need fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import './styles.css'
 
-import {ContextMenu} from "@/menu"
-import {TestModule} from "@/modules/test.module"
-import {SoundModule} from "@/modules/sound.module";
-import {BackgroundModule} from "@/modules/background.module";
+import { ContextMenu } from '@/menu'
+import { TestModule } from '@/modules/test.module'
+import { SoundModule } from '@/modules/sound.module'
+import { BackgroundModule } from '@/modules/background.module'
+import { ClicksModule } from '@/modules/clicks.module'
 
-const contextMenu = new ContextMenu("#menu")
+const contextMenu = new ContextMenu('#menu')
 
 contextMenu.add(new TestModule('test', 'Тест'))
 contextMenu.add(new SoundModule('sound', 'Случайный звук'))
 contextMenu.add(new BackgroundModule('bg', 'Случайный фон'))
+contextMenu.add(new ClicksModule('click', 'Считать клики (за 5 секунд)'))
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,43 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
+  constructor(type, text) {
+    super(type, text)
+    this.title = document.createElement('h1')
+    this.time = 5
+    this.clicks = 0
+    this.clickOnTime = true
+    this.timerId = null
+  }
 
+  clicksStart() {
+    document.body.addEventListener('click', (event) => {
+      event.preventDefault()
+      const { target } = event
+      if (this.clickOnTime) {
+        this.clicks += 1
+        console.log(this.clicks)
+      }
+    })
+  }
+
+  timer() {
+    this.timerId = setInterval(() => {
+      console.log('timer start')
+      console.log('time', this.time)
+      this.time === 0 ? this.finishTimer() : --this.time
+    }, 1000)
+  }
+
+  finishTimer() {
+    clearInterval(this.timerId)
+    this.clickOnTime = false
+    this.title.textContent = `Кликов:${this.clicks}`
+    document.body.prepend(this.title)
+  }
+
+  trigger() {
+    this.timer()
+    this.clicksStart()
+  }
 }
